c2ff0f22ad09cbfb71b3db1d0850778d72b44dbf
Ibn Ahmad <justforlearningcode@gmail.com>
2018-07-31 09:48:18 +0100

    activity_main.xml uses attribute from styles.xml

--- app/src/main/java/com/example/ibnahmad/healthcalculator/MainActivity.java
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.ibnahmad.healthcalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView mBMITextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBMITextView = findViewById(R.id.bmi_text_view);
+        mBMITextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bmi_text_view:
+                Intent startBMIActivity = new Intent(MainActivity.this, null);
+        }
     }
 }
