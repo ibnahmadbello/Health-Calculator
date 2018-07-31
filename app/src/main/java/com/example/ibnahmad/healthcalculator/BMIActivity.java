6282f1e0e99593b2c5eb81dd390c849fbed9313b
Ibn Ahmad <justforlearningcode@gmail.com>
2018-07-31 15:26:23 +0100

    Added various views for the activity_bmi.xml

--- app/src/main/java/com/example/ibnahmad/healthcalculator/BMIActivity.java
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/BMIActivity.java
@@ -6,11 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewOutlineProvider;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BMIActivity extends AppCompatActivity {
 
     private LinearLayout mLinearLayout;
+    private Button mPoundInchesButton;
+    private Button mKgMetersButton;
+    private TextView mResultTextView;
+    private EditText mPoundEditText, mInchesEditText, mKilogramEditText, mMetersEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,30 @@ public class BMIActivity extends AppCompatActivity {
 
         mLinearLayout = findViewById(R.id.bottom_details_linear_layout);
         setUpLinearOutline();
+
+        mPoundInchesButton = findViewById(R.id.bmi_kg_meters_button);
+        mPoundInchesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(BMIActivity.this, "Pound and Inches is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mKgMetersButton = findViewById(R.id.bmi_pound_inches_button);
+        mKgMetersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(BMIActivity.this, "Kilogram and Meters is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mResultTextView = findViewById(R.id.result_text_view);
+
+        mPoundEditText = findViewById(R.id.weight_in_pound_edit_text);
+        mInchesEditText = findViewById(R.id.height_in_inches_edit_text);
+        mKilogramEditText = findViewById(R.id.weight_in_kg_edit_text);
+        mMetersEditText = findViewById(R.id.height_in_meters_edit_text);
+
 
     }
 
