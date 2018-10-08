0eca122239356e7b3211c1829e931c1c8353dfe9
Ibn Ahmad <justforlearningcode@gmail.com>
2018-10-08 13:00:24 +0100

    Showing HeartRateActivity

---
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/HeartRates.java
@@ -0,0 +1,39 @@
+package com.example.ibnahmad.healthcalculator;
+
+import java.util.Date;
+
+public class HeartRates {
+
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+
+
+
+}
