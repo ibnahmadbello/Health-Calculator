98e5df445efb02151f85f89618c8db22beb4e2d1
Ibn Ahmad <justforlearningcode@gmail.com>
2018-10-09 17:02:06 +0100

    Toast displaying

--- app/src/main/java/com/example/ibnahmad/healthcalculator/HeartRatesActivity.java
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/HeartRatesActivity.java
@@ -10,9 +10,11 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.time.LocalDate;
 import java.util.Calendar;
@@ -22,9 +24,10 @@ public class HeartRatesActivity extends AppCompatActivity implements
         View.OnClickListener, DatePickerDialog.OnDateSetListener{
 
     private static final String TAG = HeartRatesActivity.class.getSimpleName();
-    private static int year0fBirth;
+    int yearOfBirth;
     private TextView mDateOfBirth;
     private EditText mFirstNameEditText, mLastNameEditText;
+    private Button showDetailButton;
     String firstName, lastName;
     int year, month, dayOfMonth;
 
@@ -35,32 +38,49 @@ public class HeartRatesActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rates);
 
+        showDetailButton = findViewById(R.id.show_heart_rate_button);
         mFirstNameEditText = findViewById(R.id.first_name_text_view);
         mLastNameEditText = findViewById(R.id.last_name_text_view);
         mDateOfBirth = findViewById(R.id.set_date_of_birth);
         View mSelectDOB = findViewById(R.id.date_of_birth_layout);
 
         mSelectDOB.setOnClickListener(this);
+        showDetailButton.setOnClickListener(this);
+
+//        getDetails();
     }
 
     @Override
     public void onClick(View view) {
-        android.support.v4.app.DialogFragment dateFragment = new DatePickerFragment();
-        dateFragment.show(getSupportFragmentManager(), "datePicker");
+        switch (view.getId()){
+            case R.id.date_of_birth_layout:
+                android.support.v4.app.DialogFragment dateFragment = new DatePickerFragment();
+                dateFragment.show(getSupportFragmentManager(), "datePicker");
+                break;
+            case R.id.show_heart_rate_button:
+                getDetails();
+                break;
+        }
+
     }
 
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
         mDateOfBirth.setText(day + "/" + (month + 1) + "/" + year);
-        year0fBirth = year;
+        yearOfBirth = year;
 
     }
 
     public void getDetails(){
         firstName = mFirstNameEditText.getText().toString();
         lastName = mLastNameEditText.getText().toString();
-        mHeartRates = new HeartRates(firstName, lastName, LocalDate.of(year, month, dayOfMonth));
-
+        mHeartRates = new HeartRates(firstName, lastName, LocalDate.of(year, month + 1, dayOfMonth + 1));
+        LocalDate todayDate = LocalDate.now();
+        int nowYear = todayDate.getYear();
+        int age = nowYear - yearOfBirth;
+        Toast.makeText(this, mHeartRates.getFirstName() + mHeartRates.getLastName() + age, Toast.LENGTH_SHORT).show();
     }
+
+
 }
