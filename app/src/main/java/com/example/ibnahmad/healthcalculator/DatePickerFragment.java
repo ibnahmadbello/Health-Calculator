06f9cbdb8830db0014ddd7d999457d96935baef0
Ibn Ahmad <justforlearningcode@gmail.com>
2018-10-09 11:45:52 +0100

    datePicker showing but no savedState

---
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/DatePickerFragment.java
@@ -0,0 +1,25 @@
+package com.example.ibnahmad.healthcalculator;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(),
+                (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+    }
+}
--- app/src/main/java/com/example/ibnahmad/healthcalculator/HeartRatesActivity.java
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/HeartRatesActivity.java
@@ -1,13 +1,87 @@
 package com.example.ibnahmad.healthcalculator;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class HeartRatesActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+
+public class HeartRatesActivity extends AppCompatActivity implements
+        View.OnClickListener{
+
+    private static final String TAG = HeartRatesActivity.class.getSimpleName();
+    private TextView mDateOfBirth;
+
+    // Selected date, stored as a timestamp
+    private long mDOB = Long.MAX_VALUE;
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rates);
+
+        mDateOfBirth = findViewById(R.id.set_date_of_birth);
+        View mSelectDOB = findViewById(R.id.date_of_birth_layout);
+
+        mSelectDOB.setOnClickListener(this);
+//        updateDateDisplay();
     }
+
+    @Override
+    public void onClick(View view) {
+        /*DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.show(getSupportFragmentManager(), TAG);*/
+        Calendar calendar = Calendar.getInstance();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                mDateOfBirth.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+            }
+        }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+
+    }
+
+    /*public long getDateSelection(){
+        return mDOB;
+    }
+
+    // Manage the selected date value
+    public void setDateSelected(long dateSelected){
+        mDOB = dateSelected;
+        updateDateDisplay();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        // Set to the date selected
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+//        calendar.set(Calendar.HOUR_OF_DAY, 0);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
+
+        setDateSelected(calendar.getTimeInMillis());
+    }
+
+    private void updateDateDisplay(){
+        if (getDateSelection() == Long.MAX_VALUE){
+            mDateOfBirth.setText(R.string.date_of_birth_not_set);
+        } else {
+            CharSequence formatted = DateUtils.getRelativeTimeSpanString(this, mDOB);
+            mDateOfBirth.setText(formatted);
+        }
+    }*/
 }
