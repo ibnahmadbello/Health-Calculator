c6043ff48884225f48d1cc0c21e3614f3dc2b9a0
Ibn Ahmad <justforlearningcode@gmail.com>
2018-10-10 14:03:42 +0100

    copy_right menu

--- app/src/main/java/com/example/ibnahmad/healthcalculator/HealthTipActivity.java
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/HealthTipActivity.java
@@ -2,6 +2,8 @@ package com.example.ibnahmad.healthcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HealthTipActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class HealthTipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_tip);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.copy_right, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.copy_right:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
