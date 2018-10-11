4252073f0d7a8f6f06bddf46c11fbea75cb8e29c
ibnahmad <justforlearningcode@gmail.com>
2018-10-11 16:58:52 +0100

    WebView

--- app/src/main/java/com/example/ibnahmad/healthcalculator/HealthTipActivity.java
+++ app/src/main/java/com/example/ibnahmad/healthcalculator/HealthTipActivity.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class HealthTipActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_tip);
+
+        webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/sample.html");
     }
 
     @Override
